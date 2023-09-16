462360b2808ccaafa7ee3718df149dbc768a2cc5
SergeySmalkov <smalkov2012@yandex.ru>
2023-09-16 17:47:08 +0300

    Context relocation to main

--- include/zmq_order_listener_updater.h
+++ include/zmq_order_listener_updater.h
@@ -15,35 +15,26 @@
 #include <thread>
 
 
-void zmq_accept_orders() {
-    zmq::context_t context(1);
+void zmq_accept_orders(zmq::context_t& context, zmq::socket_t& update_socket) {
+
     // Socket to receive orders
     zmq::socket_t order_socket(context, zmq::socket_type::pull);
     order_socket.bind("tcp://*:5555");
-    // Socket to send order updates
-    zmq::socket_t update_socket(context, zmq::socket_type::push);
-    update_socket.connect("tcp://localhost:5556");
 
     while (true) {
         zmq::message_t request;
-
         // Wait for next request from client
         order_socket.recv(&request);
         std::string order_data = std::string(static_cast<char*>(request.data()), request.size());
 
         std::cout << "Received Order: " << order_data << std::endl;
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+//        std::this_thread::sleep_for(std::chrono::seconds(1));
 
         std::string update_msg = "NEW";
         zmq::message_t update_new(update_msg.data(), update_msg.size());
         update_socket.send(update_new, zmq::send_flags::none);
 
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-
-        update_msg = "PENDING";
-        zmq::message_t update_pending(update_msg.data(), update_msg.size());
-        update_socket.send(update_pending, zmq::send_flags::none);
     }
 }
 #endif //MATCHINGENGINE_ZMQ_ORDER_LISTENER_UPDATER_H
--- main.cpp
+++ main.cpp
@@ -18,9 +18,14 @@ int main() {
     });
     ws_thread.detach();
 
+    zmq::context_t context(1);
+    // Socket to send order updates
+    zmq::socket_t update_socket(context, zmq::socket_type::push);
+    update_socket.connect("tcp://localhost:5556");
+
 
     std::thread zmq_thread([&]() {
-        zmq_accept_orders();
+        zmq_accept_orders(context, update_socket);
     });
     zmq_thread.detach();
 
